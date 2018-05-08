67df504d661cc4c25afb75792a8a63f6b8b984e1
Elryos <alex.bortho@gmail.com>
2018-05-08 13:12:12 +0200

    Cell update

--- src/Model/Cell.java
+++ src/Model/Cell.java
@@ -1,4 +1,12 @@
 package src.Model;
 
+//si la case n'est pas en bordure, border=-1
+//sinon border represente de quelle bordure il s'agit (2 bordure= dizaine+unité)
 public class Cell {
+    boolean empty;
+    int nPlayer;
+    boolean color;
+    int border = -1;
+    //ceci est du caca
+
 }
