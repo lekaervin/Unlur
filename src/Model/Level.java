5766b3c62bdc13b44faba9d9da324895fdd6e8d1
Elryos <alex.bortho@gmail.com>
2018-05-08 13:10:11 +0200

    New Class + start Cell

---
+++ src/Model/Level.java
@@ -0,0 +1,9 @@
+package src.Model;
+
+
+public class Level{
+	int mode; // J/J=1 J/AI=2 AI/AI=3
+	int level;
+	Board current;
+}
+
---
+++ src/Model/Player.java
@@ -0,0 +1,8 @@
+package src.Model;
+
+
+public class Player{
+	String name;
+	//statistiqes?
+}
+
